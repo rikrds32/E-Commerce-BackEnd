4884e3026c091ae1096d7cd311ec44a8729a9272
Ricardo Salazar <no-replay>
2021-07-28 17:59:05 -0500

    add tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,48 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Product.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name'],
+      }
+    ]
+  })
+  .then(tagsdb => res.json(tagsdb))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name'],
+      }
+    ]
+  })
+  .then(tagsdb => res.json(tagsdb))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -19,10 +56,27 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagsdb => {
+    if(!tagsdb) {
+      res.status(404).JSON({ message: 'not found'});
+      return;
+    }
+    res.JSON(tagsdb);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).JSON(err);
+  });
 });
 
 module.exports = router;
