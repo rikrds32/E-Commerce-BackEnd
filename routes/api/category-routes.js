f80f14ee027a4afd8c71efaaf0c5c59da7625fc4
Ricardo Salazar <no-replay>
2021-07-28 18:06:09 -0500

    add categoy routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,48 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Product.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name'],
+      }
+    ]
+  })
+  .then(categorydb => res.json(categorydb))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'tacategory_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name'],
+      }
+    ]
+  })
+  .then(categorydb => res.json(categorydb))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +60,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categorydb => {
+    if(!tagsdb) {
+      res.status(404).JSON({ message: 'not found'});
+      return;
+    }
+    res.JSON(categorydb);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).JSON(err);
+  });
 });
+
 
 module.exports = router;
